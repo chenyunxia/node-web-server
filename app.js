487c7c326efb20a7741741a465f1078489c30118
chenyunxia <1113972592@qq.com>
2017-01-08 12:19:02 +0800

    增加routers，ejs、jade模板

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 var	http = require('http'),
 	express = require('express'),
-	app = require('express')();
+	app = require('express')(),
+    path = require('path');
+
+var routes = require('./routers/router');
+
 
 // setting listening port
 // var server = app.listen(8090);
-app.set('port', process.env.PORT || 8090);
+app.set('port', process.env.PORT || 9090);
 
-// 设置静态资源的目录
+// 设置静态资源的目录，可同时设置多个目录，访问文件的时候会按照顺序进行查找
 app.use(express.static('public'));
+// app.use(express.static('files'));
 
+
+// setting template path
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use('/', routes);
 
 // router list
 app.get('/list', function(req, res){
   res.send({
     'code': '0',
     'msg': '成功',
-    'data': ['a', 'b', 'c', 'd', 'e', 'f']
+    'data': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
   })
 })
 
