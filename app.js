78201574df0952c9ce7d34013e272fbd1a1c723d
chenyunxia <chenyunxia@alibaba-inc.com>
2017-01-11 09:42:38 +0800

    解析不同类型的文件

---
+++ app.js
@@ -0,0 +1,29 @@
+var	http = require('http'),
+	express = require('express'),
+	app = require('express')();
+
+
+// setting listening port
+// var server = app.listen(8090);
+app.set('port', process.env.PORT || 9090);
+
+// 设置静态资源的目录，可同时设置多个目录，访问文件的时候会按照顺序进行查找
+app.use(express.static('public'));
+// app.use(express.static('files'));
+
+
+// router list
+app.get('/list', function(req, res){
+  res.send({
+    'code': '0',
+    'msg': '成功',
+    'data': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+  })
+})
+
+
+// createServer
+http.createServer(app).listen(app.get('port'), function(){  
+  console.log('listening: ' + app.get('port'));  
+});
+console.log('server running');
